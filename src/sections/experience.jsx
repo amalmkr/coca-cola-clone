c0b37aac2992c0b51a9dc6f136077329a69bbfdb
amalmkr <amalmkr01@gmail.com>
2026-07-20 16:12:11 +0530

    experience section made into mobile friendly

--- src/sections/experience.jsx
+++ src/sections/experience.jsx
@@ -8,13 +8,13 @@ function Experiance(){
             <h2 className="text-3xl font-['Archivo_Black'] text-black text-center">
             Experiences
             </h2>
-            <div className="flex p-10 justify-evenly">
-                <div className="text-left text-black mt-30 ">
+            <div className="flex flex-col-reverse md:flex-row md:p-1 justify-evenly">
+                <div className="md:text-left text-black md:mt-30 mt-10 text-center">
                     <p className="font-['Archivo_Black'] text-3xl leading-tight">Coke Studio Bharat: Drink.<br/> Scan. Unlock Exclusive<br/> Music.</p>
                     <button className="mt-6 border-2 px-15 text-xl rounded-4xl hover:bg-[#dddddd]">Explore Now</button>
                 </div>
-                <div className="">
-                    <img src={experience} className="rounded-2xl w-120 shadow-lg"/>
+                <div className="md:w-120 mt-10">
+                    <img src={experience} className="rounded-2xl shadow-lg w-90 md:w-200 mx-auto "/>
                 </div>
             </div>        
         </section>
